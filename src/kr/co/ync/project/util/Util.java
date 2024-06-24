d4b1eba82a11aa901bbee1f8728d6f37312d7157
한상희 <tkdgml822@gmail.com>
2024-06-25 00:26:45 +0900

    빈칸 삭제

--- src/kr/co/ync/project/util/Util.java
+++ src/kr/co/ync/project/util/Util.java
@@ -5,8 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 public class Util {
     public static LocalDate strToLocalDate(String date) {
+        String birth = removeWhitespace(date);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        return LocalDate.parse(date, formatter);
+        return LocalDate.parse(birth, formatter);
+    }
 
+    public static String removeWhitespace(String str) {
+        return str.replace(" ", "");
     }
 }
